a063f8ac301ba92d032f55a78a2edbcb6d832583
DavidGegic95 <davidgegic95@gmail.com>
2023-09-12 15:27:33 +0200

    done

--- src/components/ImageCarousel/ImageCarousel.jsx
+++ src/components/ImageCarousel/ImageCarousel.jsx
@@ -17,26 +17,11 @@ function ImageCarousel({ first50Shows, setSingleMovie }) {
     return (
         <Carousel animation="slide" duration="700" indicators={true} height="540px" sx={{ width: "360px" }}>
             {
-                fourShows.map((show, i) => <img onClick={() => imgOnClick(show)} key={i} className='imageCarousel' src={show.image.original}></img>)
+                fourShows.map((show, i) => <img onClick={() => imgOnClick(show)} key={i} className='imageCarousel' alt='carousel' src={show.image.original}></img>)
             }
         </Carousel>
     )
 }
 
-// function Item({ item }) {
-//     return (
-//         <Paper>
-//             <img className='imageCarousel' src={item.image.original}></img>
-//             {/* //         {/* <h2>{props.item.name}</h2>
-//         //         <p>{props.item.description}</p>
-
-//         //         <Button className="CheckButton">
-//         //             Check it out!
-//         //         </Button> */}
-//         </Paper>
-
-//     )
-
-// }
 
 export default ImageCarousel
